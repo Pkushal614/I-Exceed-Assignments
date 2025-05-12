2f15b38190f05a794f8b386ea157fbbd9b7855bb
Pkushal614 <139737428+Pkushal614@users.noreply.github.com>
2025-05-12 22:16:45 +0530

    Add files via upload

---
+++ Day2/utils/MathUtils.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class MathUtils {
+    public static int square(int x) {
+        return x * x;
+    }
+
+    public static double circleArea(double radius) {
+        return Math.PI * radius * radius;
+    }
+}
